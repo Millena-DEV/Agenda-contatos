297fcaee0cc357795c889d65d340ae834ab7bb3f
millenapoprock@gmail.com <millenapoprock@gmail.com>
2022-04-14 13:02:17 -0300

    CADASTRO
    verificar o codigo criado

--- agenda/cadastro.php
+++ agenda/cadastro.php
@@ -1,18 +1,27 @@
 <?php
 include 'conectar.php';
 
-$cod = $_POST['cod'];
-$nome = $_POST['nome'];
-$telefone = $_POST['telefone'];
-$telefone2 = $_POST['telefone2'];
-$email = $_POST['email'];
-$email2 = $_POST['email2'];
+$cliente = $_POST['nome'];
+$endereco = $_POST ['logadouro,cep,numero,cidade,estado,bairro'];
+$Tipo_Contato = $_POST ['email,telefone'];
+$PessoaFisica = $_POST ['cpf'];
+$PessoaJuridica = $_POST ['cnpj'];
 
-$recebendo_cadastros = "INSERT INTO contatos
 
-    (nome, email, email2, telefone, telefone2)
+$recebendo_Endereco = "INSERT INTO Endereco (logadouro,cep,numero,cidade,estado,bairro)
+VALUES ('$logadouro','$cep','$numero','$cidade,'$estado','$bairro')";
 
-    VALUES ('$nome', '$email', '$email2', '$telefone', '$telefone2')";
+$recebendo_PessoaFisica  = "INSERT INTO PessoaFisica (cpf) 
+VALUES ('$cpf')";
+
+$recebendo_PessoaJuridica  = "INSERT INTO PessoaJuridica (cnpj) 
+VALUES ('$cnpj')";
+
+$recebendo_Tipo_Contato= "INSERT INTO Tipo_Contato (email,telefone) 
+VALUES ('$email','$telefone')";
+
+$recebendo_cadastros = "INSERT INTO Cliente(nome,idEndereco,idTipo_Contato,idPessoa_Fisica,idPessoa_Juridica)
+ VALUES (LAST_INSERT_ID(),'$nome','$email','$telefone','$cpf','$cnpj','$logadouro','$cep','$numero','$cidade,'$estado','$bairro')";
 
 $query_cadastros = pg_query($con, $recebendo_cadastros) or die(pg_last_error($con));
 
